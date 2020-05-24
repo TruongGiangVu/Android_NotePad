9c840a8cf9851fa0c53a6034e9b2a8bdfe979733
TruongGiangVu <45998623+TruongGiangVu@users.noreply.github.com>
2020-05-24 11:23:20 +0700

    NotepadActivity commit

--- app/src/main/java/com/giangvu/notepad/NotepadActivity.java
+++ app/src/main/java/com/giangvu/notepad/NotepadActivity.java
@@ -5,13 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class NotepadActivity extends AppCompatActivity {
 
